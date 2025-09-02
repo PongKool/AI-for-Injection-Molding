22c0127ce7103dc20cb0ede32da5b838a6c99108
pingpong <pong111@hotmail.com>
2025-09-02 18:49:30 +0700

    edit predict_manual.py to suppress warnings and TensorFlow logs

--- predict_manual.py
+++ predict_manual.py
@@ -2,11 +2,19 @@
 # Manual prediction loop with auto-suggestion
 # If predicted Part Weight > 50 g, search for new inputs that yield < 50 g
 
+# Suppress warnings for cleaner output
 
-# Disable GPU for simplicity (optional)
+import warnings
+warnings.filterwarnings("ignore")
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow warnings
 
+
+
+# Disable GPU for simplicity (optional) by comment out the next two lines
 # import os
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 import numpy as np
 import tensorflow as tf
